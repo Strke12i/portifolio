acf9335353e617da1a1b97e6284a00101835800c
Strke12i <felipebagesteiro@gmail.com>
2025-02-16 23:20:43 -0300

    add files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Felipe Colpo Bagesteiro - Data Engineer</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
